dbc44e7e889aa976b10f5bd57fcf8c0777ceaace
Harsh Yadav <hy129794@gmail.com>
2026-05-10 23:18:43 +0530

    Initial ecommerce project

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const path=require("path");
 const ejsMate=require("ejs-mate");
 const methodOverride=require("method-override");
 const app=express();
-const port=8080;
-app.listen(port,()=>{
-    console.log(`server is listining at port ${port}`);
-});
-app.set("view engine","ejs");
-app.use(methodOverride("_method"));
-app.set("views",path.join(__dirname,"views"));
+const port=process.env.PORT || 8080;
+
 app.engine("ejs",ejsMate);
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+app.listen(port,()=>{
+    console.log(`server is listening at port ${port}`);
+});
 
 
 //database
@@ -33,24 +37,92 @@ app.get("/",(req,res)=>{
 
 //home route
 app.get("/home",async(req,res)=>{
-    let listings=await Listing.find({});
-    res.render("allPages/home.ejs",{listings});
+    try {
+        let listings=await Listing.find({});
+        res.render("allPages/home.ejs",{listings});
+    } catch (err) {
+        console.log(err);
+        res.send("Error loading listings");
+    }
 });
 
 //show route
 app.get("/show/:id",async(req,res)=>{
-    let {id}=req.params;
-    let listing=await Listing.findById(id);
-    res.render("allPages/show.ejs",{listing});
+    try {
+        let {id}=req.params;
+        let listing=await Listing.findById(id);
+        if (!listing) {
+            return res.send("Listing not found");
+        }
+        res.render("allPages/show.ejs",{listing});
+    } catch (err) {
+        console.log(err);
+        res.send("Error loading listing");
+    }
 });
 
 
 //edit route
 app.get("/edit/:id",async(req,res)=>{
-    let {id}=req.params;
-    let listing=await Listing.findById(id);
-    res.render("allPages/edit.ejs",{listing});
-})
+    try {
+        let {id}=req.params;
+        let listing=await Listing.findById(id);
+        if (!listing) {
+            return res.send("Listing not found");
+        }
+        res.render("allPages/edit.ejs",{listing});
+    } catch (err) {
+        console.log(err);
+        res.send("Error loading listing");
+    }
+});
+
+//new route
+app.get("/new",(req,res)=>{
+    res.render("allPages/new.ejs");
+});
+
+//create route
+app.post("/",async(req,res)=>{
+    try {
+        let newListing=new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/home");
+    } catch (err) {
+        console.log(err);
+        res.send("Error creating listing");
+    }
+});
+
+//update route
+app.put("/:id",async(req,res)=>{
+    try {
+        let {id}=req.params;
+        let listing = await Listing.findByIdAndUpdate(id,{...req.body.listing});
+        if (!listing) {
+            return res.send("Listing not found");
+        }
+        res.redirect(`/show/${id}`);
+    } catch (err) {
+        console.log(err);
+        res.send("Error updating listing");
+    }
+});
+
+//delete route
+app.delete("/:id",async(req,res)=>{
+    try {
+        let {id}=req.params;
+        let listing = await Listing.findByIdAndDelete(id);
+        if (!listing) {
+            return res.send("Listing not found");
+        }
+        res.redirect("/home");
+    } catch (err) {
+        console.log(err);
+        res.send("Error deleting listing");
+    }
+});
 
 
 
